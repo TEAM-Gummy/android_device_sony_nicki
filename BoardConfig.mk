3c20cf19d9c4973c6a9885da59e5dfe074361838
Arnav Gosain <nolinuxnoparty@gmail.com>
2014-08-11 13:17:58 -0400

    Refactor BoardConfig
    Change-Id: I31dbab5d7c22cde1de7e81ae4d14931863aca322

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,20 +26,21 @@ USE_CAMERA_STUB := false
 
 TARGET_SPECIFIC_HEADER_PATH += device/sony/nicki/include
 
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
-COMMON_GLOBAL_CFLAGS += -DSONY_CAM_PARAMS
-
 # Platform
 TARGET_BOARD_PLATFORM := msm8960
 TARGET_CPU_VARIANT := krait
 BOARD_VENDOR_PLATFORM := nicki
 TARGET_BOOTLOADER_BOARD_NAME := qcom
 
-# Asserts
+# OTA Assert
 TARGET_OTA_ASSERT_DEVICE := c1904,c1905,nicki
+
+# Cflags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+COMMON_GLOBAL_CFLAGS += -DSONY_CAM_PARAMS
 
 # Kernel
 BOARD_KERNEL_CMDLINE := panic=3 console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
@@ -47,10 +48,9 @@ BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 TARGET_KERNEL_SOURCE := kernel/sony/msm8x27
-TARGET_KERNEL_CONFIG := nicki_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := Linaro-4.7
+TARGET_KERNEL_CONFIG := cyanogenmod_nicki_defconfig
 
-# Filesystems
+# Partitions
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
@@ -62,16 +62,18 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_VOLD_MAX_PARTITIONS := 27
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
-# Graphics
+# QCOM Hardware
 BOARD_USES_QCOM_HARDWARE := true
-TARGET_QCOM_AUDIO_VARIANT := caf
+
+# QCOM Display and Graphics
+BOARD_EGL_CFG := device/sony/nicki/rootdir/system/lib/egl/egl.cfg
 TARGET_QCOM_DISPLAY_VARIANT := caf
 TARGET_QCOM_MEDIA_VARIANT := caf
 TARGET_USES_QCOM_BSP := true
 TARGET_DISPLAY_USE_RETIRE_FENCE := true
-BOARD_EGL_CFG := device/sony/nicki/rootdir/system/lib/egl/egl.cfg
 
-# audio
+# Audio
+TARGET_QCOM_AUDIO_VARIANT := caf
 BOARD_USES_ALSA_AUDIO := true
 BOARD_USES_LEGACY_ALSA_AUDIO := true
 TARGET_USES_QCOM_COMPRESSED_AUDIO := true
@@ -79,7 +81,10 @@ TARGET_USES_QCOM_COMPRESSED_AUDIO := true
 # QC AV Enhancements
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
-# camera
+# QC Time
+BOARD_USES_QC_TIME_SERVICES := true
+
+# Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
 
 # Fm
@@ -112,15 +117,12 @@ BOARD_CUSTOM_BOOTIMG_MK := device/sony/nicki/custombootimg.mk
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/sony/nicki/recovery/recovery_keys.c
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 
-# QC Time Services
-BOARD_USES_QC_TIME_SERVICES := true
-
-# Override healthd HAL
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.qcom
-
 # PowerHAL
 TARGET_POWERHAL_VARIANT := qcom
 TARGET_POWERHAL_NO_TOUCH_BOOST := true
+
+# Override healthd HAL
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.qcom
 
 # TWRP
 DEVICE_RESOLUTION := 480x854
